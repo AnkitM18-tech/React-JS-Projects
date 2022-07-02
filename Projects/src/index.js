c9f9d8e49e925457ce600a228378855a498ef570
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-07-02 18:41:16 +0530

    Tours Project Completed

--- Projects/src/index.js
+++ Projects/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./birthday-reminder/index.css";
+// import "./birthday-reminder/index.css";
+import "./tours/index.css";
 import App from "./App";
 
 ReactDOM.render(
