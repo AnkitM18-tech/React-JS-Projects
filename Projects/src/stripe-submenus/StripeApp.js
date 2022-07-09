df3f52b945e4750c364b5de6df28b7b5aa35e7d5
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-07-09 23:07:22 +0530

    Stripe Menu Project Completed

---
+++ Projects/src/stripe-submenus/StripeApp.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
+function StripeApp() {
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
+  );
+}
+
+export default StripeApp;
