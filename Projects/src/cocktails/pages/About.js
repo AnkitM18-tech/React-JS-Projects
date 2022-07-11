81002662d12e7fd4acf535bf69a3e7f2a7240045
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2022-07-11 17:21:53 +0530

    About Page

--- Projects/src/cocktails/pages/About.js
+++ Projects/src/cocktails/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus sint
+        quisquam vero! Doloremque officia voluptate ullam vitae dolor eos rerum
+        adipisci inventore dolore non. Cum sapiente praesentium consequatur ad
+        provident libero, soluta obcaecati commodi quas ducimus, molestiae
+        reiciendis omnis sint.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
